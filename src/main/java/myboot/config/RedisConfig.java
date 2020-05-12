61475e7e281a52268967ef55b290a30ce14db768
杨曙光 <yangshuguang@yunquna.com>
2020-05-12 16:05:11 +0800

    redis  and threadpool

--- src/main/java/myboot/config/RedisConfig.java
+++ src/main/java/myboot/config/RedisConfig.java
@@ -2,24 +2,32 @@ package myboot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-
-
     @Bean(name = "redisTemplate")
     @Primary
-    public RedisTemplate<Object, Object> redisTemplate() {
-        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisStandaloneConfiguration configuration=new RedisStandaloneConfiguration();
+        configuration.setPassword("123456");
+        configuration.setHostName("127.0.0.1");
+        configuration.setPort(6379);
+        LettuceConnectionFactory connectionFactory =new LettuceConnectionFactory(configuration);
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
+        template.setConnectionFactory(connectionFactory);
         return template;
     }
 }
--- src/main/java/myboot/service/TestService.java
+++ src/main/java/myboot/service/TestService.java
@@ -4,13 +4,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.nio.ch.ThreadPool;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class TestService {
+
     @Autowired
-    private RedisTemplate<String,Object> redisTemplate;
+    RedisTemplate<String, String> redisTemplate;
+
     @GetMapping("/test")
-    public String  test(){
-        return "111";
+    public String test() {
+        redisTemplate.opsForValue().set("test", "ysg");
+
+        return redisTemplate.opsForValue().get("test");
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        Semaphore semaphore = new Semaphore(3);
+        AtomicInteger test = new AtomicInteger();
+        for (int i = test.get(); i < 100; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        if(test.get()%3==0){
+                            Thread.sleep(1000);
+                        }
+                        System.out.println("第" + test.getAndAdd(1) + "次调用");
+                    } catch (InterruptedException t) {
+                        System.out.println("第" + test.get() + "次调用失败");
+                    } finally {
+                        semaphore.release();
+                    }
+                }
+            });
+        }
     }
 }
