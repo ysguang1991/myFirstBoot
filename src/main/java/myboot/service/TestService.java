ddc02d0bcc79c7c581d665424591c66028b9718a
杨曙光 <yangshuguang@yunquna.com>
2020-05-25 21:51:56 +0800

    注册中心

--- src/main/java/myboot/service/TestService.java
+++ src/main/java/myboot/service/TestService.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@RestController
+
 public class TestService {
 
     @Autowired
