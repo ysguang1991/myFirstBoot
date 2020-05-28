ed8dd39a0fe8f2bf8c6371f05186ca9405170115
杨曙光 <yangshuguang@yunquna.com>
2020-05-28 19:56:47 +0800

    去掉注册中心

--- service/src/main/java/myboot/ServiceApplication.java
+++ service/src/main/java/myboot/ServiceApplication.java
@@ -2,11 +2,11 @@ package myboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
-@EnableEurekaServer
+//EnableEurekaServer
 public class ServiceApplication {
     public static  void main(String[] args){
         SpringApplication.run(ServiceApplication.class,args);
